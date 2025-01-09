4ade742a3f2b13a2586af95e3c94fe507636a301
USER <USER@MUSTAFA>
2025-01-09 03:03:03 +0300

    login and register

--- WeServe/obj/Debug/net8.0/WeServe.AssemblyInfo.cs
+++ WeServe/obj/Debug/net8.0/WeServe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeServe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b2f709dcafb4bb2b1a4787c6163343fa79a7db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6a78972f3afed79ec8f60e0e831bda5367ae67")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeServe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeServe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
