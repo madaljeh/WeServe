bca99a453236e45a46141cd75675e84aa44140c9
madaljeh <ahmadmadaljeh@gmail.com>
2025-01-09 19:36:53 +0300

    new

--- WeServe/obj/Debug/net8.0/WeServe.AssemblyInfo.cs
+++ WeServe/obj/Debug/net8.0/WeServe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeServe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68de5f6bd3777cb2373a7c7c64058b6caa203e4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b955234b294612e9654fdd013837313e78da0dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeServe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeServe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
